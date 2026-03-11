9cc147f9394aa132d99270de48705e42c77454e7
olgarednbd <olgazerocod@gmail.com>
2026-03-11 14:54:23 +0700

    add new func summa add new file test

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-Egor
-def area_circule(r):
-    S = 3.14 * r**2
-    return S
-
-
-def area_square(a):
-    S_s = a**2
-    return S_s
-
-
-def area_trapecoid(a,b,h):
-    S_t = 0.5 * (a + b) * h
-    return S_t
+#Egor
+def summa(a, b):
+    print(a + b)
