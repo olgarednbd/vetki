aedda59a6b460e7801084821b36a2f64fff4dc45
olgarednbd <olgazerocod@gmail.com>
2026-03-11 12:19:50 +0700

    correct main

--- mai.py
+++
@@ -1,5 +0,0 @@
-def area_circule(r):
-    S = 3.14 * r ** 2
-    return Sdef area_square(a):
-    S_s = a**2
-    return S_s
---
+++ main.py
@@ -0,0 +1,7 @@
+def area_circule(r):
+    S = 3.14 * r**2
+    return S
+
+def area_square(a):
+    S_s = a**2
+    return S_s
