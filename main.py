e5e6e49b292cc1d937fea3ac2bfafd9464e86750
olgarednbd <olgazerocod@gmail.com>
2026-03-11 14:27:47 +0700

    add ne func calc

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-def area_circule(r):
-    S = 3.14 * r**2
-    return S
-
-
-def area_square(a):
-    S_s = a**2
-    return S_s
-
-
-def area_trapecoid(a,b,h):
-    S_t = 0.5 * (a + b) * h
-    return S_t
+def calc(a, b):
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a/b)
